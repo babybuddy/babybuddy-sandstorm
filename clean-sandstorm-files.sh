5b68c8a54f449caffe0a1e4cba4ad4999faf80a6
Christopher C. Wells <chris@chrxs.net>
2020-02-08 14:18:20 -0800

    Add bash script to clean Sandstorm files list

---
+++ clean-sandstorm-files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Removes entries in .sandstorm/sandstorm-files.list that are already part of
+# the "alwaysInclude" configuration option. This is primary done to keep the
+# files list cleaner and easier to parse/update/understand.
+#
+# Paths with the following directories are removed from the file:
+#  - opt/app/babybuddy
+#  - usr/lib/python3
+#  - usr/lib/python3.7
+#
+# This script should be run from the repo root.
+
+sed -i "/\b\(opt\/app\/babybuddy\|usr\/lib\/python3\)\b/d" .sandstorm/sandstorm-files.list
