41faf37b06424d5079fb676faf58123359ec0b2a
Christopher C. Wells <chris@chrxs.net>
2020-02-09 14:31:15 -0800

    Maintain .env config between builds

--- .sandstorm/build.sh
+++ .sandstorm/build.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 set -euo pipefail
 
+# Save or create an .env file.
+if [ -f /opt/app/babybuddy/.env ]; then
+  mv /opt/app/babybuddy/.env /opt/app
+else
+  SECRET_KEY=$(python -c 'import random; result = "".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)]); print(result)')
+  cat <<EOF > /opt/app/.env
+DJANGO_SETTINGS_MODULE=babybuddy.settings.sandstorm
+SECRET_KEY=$SECRET_KEY
+EOF
+fi
+
 # Get application code.
 cd /opt/app
 rm -rf babybuddy
 git clone https://github.com/babybuddy/babybuddy.git
-
-# Set up required application environment variables.
-SECRET_KEY=$(python -c 'import random; result = "".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)]); print(result)')
-cat <<EOF > /opt/app/babybuddy/.env
-DJANGO_SETTINGS_MODULE=babybuddy.settings.sandstorm
-SECRET_KEY=$SECRET_KEY
-EOF
 
 # Checkout desired version and apply Sandstorm patch.
 VERSION=$(grep -oE 'appMarketingVersion = \(defaultText = "(.+)"' /opt/app/.sandstorm/sandstorm-pkgdef.capnp | cut -d '"' -f2)
@@ -19,6 +23,9 @@ cd /opt/app/babybuddy
 git add --all
 git checkout -f v"${VERSION}"
 git apply /opt/app/sandstorm.patch
+
+# Move .env file in to application directory.
+mv /opt/app/.env /opt/app/babybuddy/.env
 
 # Set up virtual environment.
 VENV=/opt/app/babybuddy/.venv
